a092ffa9333893260cda4a177624fb452914d871
habib daou <36645987+DaouHabib@users.noreply.github.com>
2021-04-18 03:01:43 +0200

    commit

--- multimarker/js/multiMarkersName.js
+++ multimarker/js/multiMarkersName.js
@@ -60,20 +60,3 @@ AFRAME.registerComponent('registerevents', {
 		},
 	});
 
-AFRAME.registerComponent('vidhandler', {
-		init: function () {
-		  this.toggle = false;
-		  document.querySelector("#BS0i6").pause(); //reference to the video
-		},
-		tick:function(){  
-		 if(document.querySelector("a-marker").object3D.visible == true){
-		   if(!this.toggle){
-			 this.toggle = true;
-			 document.querySelector("#BS0i6").play();
-		   }
-		 }else{
-		   this.toggle = false;
-		   document.querySelector("#BS0i6").pause();
-		 }
-		}
-	  });
